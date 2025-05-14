1b66e18992f22eec0ec4a4019dc2ab5d425a0d07
Lk <lukachanturishvili05@gmail.com>
2025-05-14 15:18:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -174,3 +174,80 @@ openPrivacyBtn.addEventListener("click", () => {
 closePrivacyBtn.addEventListener("click", () => {
   privacyOverlay.style.display = "none";
 });
+// Google Analytics
+// Initialize Google Analytics with your tracking ID (replace with your actual GA ID)
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-TXZ4KQ5W8P'); // Google Analytics ID
+
+// Amplitude
+// Initialize Amplitude with your project ID and session replay plugin
+(function(e, t){
+  var n=e.amplitude||{_q:[],_iq:{}};
+  var r=t.createElement("script");r.type="text/javascript";r.async=true;
+  r.src="https://cdn.amplitude.com/libs/amplitude-2.16.1-min.js";
+  r.onload=function(){e.amplitude.runQueuedFunctions()};
+  var i=t.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i);
+  var s=function(t,n){e.amplitude[t]=function(){e.amplitude._q.push([t].concat(Array.prototype.slice.call(arguments,0)))}};
+  var o=["add","append","clearAll","setUserId","setUserProperties","logEvent"];
+  for(var a=0;a<o.length;a++){s(o[a])}
+})(window, document);
+
+// Initialize Amplitude with your project ID and session replay plugin
+window.amplitude.add(window.sessionReplay.plugin({sampleRate: 1}));
+window.amplitude.init('8c8d9e3d7080e5bfd91a608b6f703256', {
+  fetchRemoteConfig: true,
+  autocapture: true,
+  serverZone: "EU" // If you are using the EU zone
+});
+
+// Track page load with Amplitude
+window.amplitude.track('page_loaded');
+
+// Example: Track when the user clicks the 'Battle Start' button
+document.getElementById('battleBtn').addEventListener('click', function() {
+  window.amplitude.track('battle_started', {
+    playerOne: 'player1Data',
+    playerTwo: 'player2Data'
+  });
+});
+
+// Example: Track nickname input
+document.getElementById('nicknameInput').addEventListener('input', function(event) {
+  window.amplitude.logEvent('nickname_input', { nickname: event.target.value });
+});
+const db = firebase.firestore();
+const userCountRef = db.collection('user_count').doc('active_users');
+
+// Update the active user count
+function updateUserCount(increment = true) {
+  userCountRef.update({
+    count: firebase.firestore.FieldValue.increment(increment ? 1 : -1)
+  });
+}
+
+// Track user entering the site
+window.addEventListener('load', () => {
+  updateUserCount(true);
+});
+
+// Track user leaving the site
+window.addEventListener('beforeunload', () => {
+  updateUserCount(false);
+});
+// Track user activity every second
+setInterval(() => {
+  window.amplitude.logEvent('user_session_active', {
+    'event_category': 'User Activity',
+    'event_label': 'User is active on the website'
+  });
+}, 1000);  // Sends data every second to Amplitude
+
+// Initialize Amplitude with your project ID
+window.amplitude.init('8c8d9e3d7080e5bfd91a608b6f703256', {
+  fetchRemoteConfig: true,
+  autocapture: true,
+  serverZone: "EU"  // Use EU server zone if necessary
+});
+
